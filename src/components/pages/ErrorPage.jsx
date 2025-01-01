e6559dd1961fc0a597227df44d3aaab22182a9f7
HeyYouYesYou <andriy.yepik@gmail.com>
2025-01-01 17:02:43 +0100

    fix(ErrorPage.jsx): now catch all errors
    use hook useRouteError and notFound page don't need

--- src/components/pages/ErrorPage.jsx
+++ src/components/pages/ErrorPage.jsx
@@ -1,6 +1,7 @@
 import { NavLink, useRouteError } from "react-router-dom";
 
 import { Title } from "../templates/Title";
+import { Subtitle } from "../templates/Subtitle";
 
 export default function ErrorPage() {
   const error = useRouteError();
@@ -10,9 +11,10 @@ export default function ErrorPage() {
       <div className="max-w-full opacity-0 bg-white border-redDark border-[1px] p-8 rounded-sm flex flex-col items-end animate-fadeIn">
         <Title>
           {error.status == "404"
-            ? "404 Page Not Found"
-            : "Oops, something went wrong..."}
+            ? "Page Not Found"
+            : "Oops, something went wrong on the server..."}
         </Title>
+        {error.status && <Subtitle>{`Error: ${error.status}`}</Subtitle>}
         <button className="bg-Lime font-medium  border-[1px] transition-all mt-8 rounded-sm max-w-fit px-4 py-2 text-grayLight hover:bg-transparent hover:text-Lime hover:border-[1px] border-Lime">
           <NavLink to="/home">go to Home page</NavLink>
         </button>
